5d09228d483fc3eba7842d180ff7c3acaedb35b4
João Gustavo Medeiros Pontes <joaogustavomp.jgm@gmail.com>
2022-09-30 14:39:50 -0300

    Update Program.cs

--- AED - Algoritmos e Estruturas de Dados/Laboratório 06/Quick Sort/Program.cs
+++ AED - Algoritmos e Estruturas de Dados/Laboratório 06/Quick Sort/Program.cs
@@ -4,9 +4,57 @@ namespace Quick_Sort
 {
     class Program
     {
+        static void criaVetor(int[] vetor)
+        {
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Random rand = new Random();
+                vetor[i] = rand.Next(0, 10);
+            }
+        }
+        static void swap(int[] vetor, int inicio, int tam)
+        {
+            int aux = vetor[inicio];
+            vetor[inicio] = vetor[tam];
+            vetor[tam] = aux;
+        }
+        static void quickSort(int[] vetor, int inicio, int tam)
+        {
+            if (tam > inicio)
+            {
+                int pivo = particiona(vetor, inicio, tam);
+                quickSort(vetor, inicio, pivo - 1);
+                quickSort(vetor, pivo + 1, tam);
+            }
+        }
+        static int particiona(int[] vetor, int inicio, int tam)
+        {
+            int pivo = vetor[tam];
+            int i = (inicio - 1);
+            for (int j = inicio; j <= tam - 1; j++)
+            {
+                if (vetor[j] < pivo)
+                {
+                    i++;
+                    swap(vetor, i, j);
+                }
+            }
+            swap(vetor, i + 1, tam);
+            return (i + 1);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int tam = 10;
+            int[] vetor = new int[tam];
+            criaVetor(vetor);
+            Console.WriteLine("\n================================");
+            Console.WriteLine("Quick Sort");
+            Console.Write("\nO vetor: ");
+            for (int i = 0; i < vetor.Length; i++) Console.Write("{0} ", vetor[i]);
+            Console.Write("\nOrdenado: ");
+            quickSort(vetor, 0, tam - 1);
+            for (int i = 0; i < vetor.Length; i++) Console.Write("{0} ", vetor[i]);
+            Console.WriteLine("\n================================\n");
         }
     }
 }
