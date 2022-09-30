855e24512d29e43bfae9c5055f74b7c1b1ccbff2
João Gustavo Medeiros Pontes <joaogustavomp.jgm@gmail.com>
2022-09-30 02:30:54 -0300

    Shell Sort

--- AED - Algoritmos e Estruturas de Dados/Laboratório 06/Shell Sort/Program.cs
+++ AED - Algoritmos e Estruturas de Dados/Laboratório 06/Shell Sort/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Shell_Sort {
+    class Program
+    {
+        static void criaVetor(int []vetor) {
+            for (int i = 0; i < vetor.Length; i++) {
+                Random rand = new Random();
+                vetor[i] = rand.Next(0,10);
+            }
+        }
+        static void shellSort(int []vetor) {
+            for (int gap = vetor.Length/2; gap > 0; gap /= 2)
+            {
+                for (int i = gap; i < vetor.Length; i += 1)
+                {
+                    int aux = vetor[i];
+                    int j;
+                    for (j = i; j >= gap && vetor[j - gap] > aux; j -= gap) vetor[j] = vetor[j - gap];
+                    vetor[j] = aux;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            const int tam = 10;
+            int []vetor = new int[tam];
+            criaVetor(vetor);
+            Console.WriteLine("\n================================");
+            Console.WriteLine("Shell Sort");
+            Console.Write("\nO vetor: ");
+            for (int i = 0; i < vetor.Length; i++) Console.Write("{0} ", vetor[i]);
+            Console.Write("\nOrdenado: ");
+            shellSort(vetor);
+            for (int i = 0; i < vetor.Length; i++) Console.Write("{0} ", vetor[i]);
+            Console.WriteLine("\n================================\n");
+        }
+    }
+}
